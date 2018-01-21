31f3e6e01627f80fc3a464037823c5555d33054d
ShivaShankariG <shivguru06@gmail.com>
2018-01-21 12:43:54 +0530

    My drive dropdown

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import FlatButton from 'material-ui/FlatButton';
-
+import DetailsIcon from 'material-ui/svg-icons/image/details';
 export default class NavBar extends React.Component
 {
     render()
@@ -9,10 +9,15 @@ export default class NavBar extends React.Component
             <div className="navbarleftdiv">
                 <ul>
 
-                    <li className="navbarlistitem" ><FlatButton label="New" /></li>
-                    <li className="navbarlistitem" ><FlatButton label="New" /> </li>
-                    <li className="navbarlistitem" ><FlatButton label="New" /> </li>
-                    <li className="navbarlistitem" ><FlatButton label="New" /> </li>
+                    <li className="navbarlistitem" ><FlatButton className= "newbutton" label="New" backgroundColor="#2962FF" /></li>
+                    <li className="navbarlistitem"> 
+                    <FlatButton className= "dropbutton"
+                        label="My drive"
+                        labelPosition="before"
+                        primary={true}
+                        icon={<DetailsIcon style={{}}/>}
+                        />
+                    </li>
                 </ul>
             </div>
         );
