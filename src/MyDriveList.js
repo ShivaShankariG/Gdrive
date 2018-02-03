57daca6c145a708ecc113c86dcfe53e9968728ad
ShivaShankariG <shivguru06@gmail.com>
2018-02-03 23:09:36 +0530

    download works

--- src/MyDriveList.js
+++ src/MyDriveList.js
@@ -11,6 +11,8 @@ import FolderIcon from './images/GDocs.png';
 import {getLoggedInUser,downloadFile, getFoldersOfUser,loginUser,getFolderList,getPromise, getDetails,getDetailsofFolders,getDetailsofFiles} from './login';
 import Paper from 'material-ui/Paper';
 import MyMenu from './MyMenu';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
 const styles= {
   height: 600,
   marginTop: 130,
@@ -21,9 +23,11 @@ const styles= {
  
 }
 
-/*the array to get the folderInfo*/
 
-
+//a function to give the details of selected row to be displayed in the side bar.
+export function setSelectedRowDetails(data){
+  return data;
+}
 
 export default class MyDriveList extends React.Component{
   constructor(props) {
@@ -33,18 +37,19 @@ export default class MyDriveList extends React.Component{
       hvPwd: this.props.hvPwd,
       TData: [{}], /*Array to hold the data from fetch */
       success: false,
-      selected: [1]
+     // selected: [1]
    
     };
 
     this.componentDidMount = this.componentDidMount.bind(this);
-    this.handleRowSelection = this.handleRowSelection.bind(this);
-    this.isSelected = this.isSelected.bind(this);
+   // this.handleRowSelection = this.handleRowSelection.bind(this);
+ //   this.isSelected = this.isSelected.bind(this);
+    this.handleSelection=this.handleSelection.bind(this);
   }
 
-  isSelected = (index) => {
+  /*isSelected = (index) => {
     return this.state.selected.indexOf(index) !== -1;
-  };
+  };*/
 
   handleRowSelection = (selectedRows) => {
     this.setState({
@@ -57,16 +62,24 @@ export default class MyDriveList extends React.Component{
 
  /*the getLoggedInUser returning nothing here. How to get the rtpthid?*/
  handleSelection(selectedRow){
-
+   
    alert("row selected: "+selectedRow);
    const TData = this.state.TData;
+   setSelectedRowDetails(TData[selectedRow]);
    console.log('file_id of row '+ selectedRow +' -> '+ TData[selectedRow]["file_id"] );
       var file_id="";
+      file_id=TData[selectedRow]["file_id"] ;
+      if(!file_id)
+      {
+        alert("select a file please");
+        return;
+      }
+      var file_name=TData[selectedRow]["file_name"] ;
+
       var auth_token=getLoggedInUser().token;
       console.log("auth_token is "+auth_token);
-      file_id=TData[selectedRow]["file_id"] ;
       console.log("file_id is "+file_id)
-      downloadFile(file_id, auth_token);
+      downloadFile(file_id,file_name, auth_token);
   }
   componentDidMount() {
     alert("reached componentDidMount");
@@ -167,7 +180,7 @@ export default class MyDriveList extends React.Component{
           </TableHeader>
           <TableBody  displayRowCheckbox={false}>
                   {this.state.TData.map( (row, index) => (
-                    <TableRow  key={index} selected={this.isSelected(index)}>
+                    <TableRow  key={index} >
                       <TableRowColumn>{row.path_nm ?<img src={FolderIcon} alt="folder" height='20' width='30'/>: null}</TableRowColumn>
                       <TableRowColumn>{row.path_nm ?row.path_nm : row.file_name}</TableRowColumn>
                       <TableRowColumn>{uName}</TableRowColumn>
