2ecb3a9f5c3083a0530b6d7c40377bbf354816d7
ShivaShankariG <shivguru06@gmail.com>
2018-02-16 21:41:17 +0530

    added loadTable() in myDriveList

--- src/MyDriveList.js
+++ src/MyDriveList.js
@@ -11,6 +11,9 @@ import FileIcon from './images/GDocs.png';
 import FolderIcon from './images/folder.png';
 import {getLoggedInUser,setLoggedInUser,downloadFile,getDetailsofFolders,getDetailsofFiles,getPromiseOfUploadFile,getPromiseOfFolderInfoUpdate} from './login';
 import Paper from 'material-ui/Paper';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+
 const styles= {
   position: 'absolute',
   height: 600,
@@ -25,16 +28,31 @@ export default class MyDriveList extends React.Component{
   constructor(props) {
     super(props);
     this.state = {
-      hvName: this.props.hvName,
-      hvPwd: this.props.hvPwd,
       TData: [{}],
-      success: false,
      }
     this.componentDidMount = this.componentDidMount.bind(this);
     this.handleSelection=this.handleSelection.bind(this);
-
+    this.loadTable = this.loadTable.bind(this);
   }
-  
+  loadTable(pathid)
+  {
+    var data = {
+      hvfldrid: pathid
+      } 
+      var arrayFiles =  [{}];
+      var arrayMix = [{}];
+       getDetailsofFolders(data).then( (tableData) => {arrayFiles = tableData[0];
+       getDetailsofFiles(data).then( (fileData) => {
+        var file=fileData[0];
+        arrayMix = arrayFiles.concat(file);
+     
+        this.setState({ TData: arrayMix} ); 
+        console.log("set state called");
+       
+       })
+      } )
+      return
+  }
  handleSelection(selectedRow){
   const TData = this.state.TData;
      var file_id="";
@@ -43,21 +61,10 @@ export default class MyDriveList extends React.Component{
      {
        var fldrid= TData[selectedRow]["path_id"] ;
        if(fldrid){
-         var userDetails = getLoggedInUser();
-         userDetails.rtpthid = fldrid;
-         setLoggedInUser(userDetails.userName, 
-                         userDetails.token, 
-                         userDetails.rtpthid, 
-                         userDetails.hasura_id);
-         userDetails = getLoggedInUser();
-         this.componentDidMount();
-         return;
+        this.loadTable(fldrid);
        }
-       alert("select a file please");
-       return;
      }
      var file_name=TData[selectedRow]["file_name"] ;
-
      var auth_token=getLoggedInUser().token;
      console.log("auth_token is "+auth_token);
      console.log("file_id is "+file_id)
@@ -66,44 +73,16 @@ export default class MyDriveList extends React.Component{
  
  
  componentDidMount() {
-   //alert("componentDidMount");
-   var data = {
-       hvfldrid: getLoggedInUser().rtpthid
-       } 
-       var arrayFiles =  [{}];
-       var arrayMix = [{}];
-        getDetailsofFolders(data).then( (tableData) => {arrayFiles = tableData[0];} )
-        getDetailsofFiles(data).then( (fileData) => {
-         var file=fileData[0];
-         arrayMix = arrayFiles.concat(file);
+   
+  this.loadTable(getLoggedInUser().rtpthid);
       
-         this.setState({ TData: arrayMix} ); 
-         console.log("set state called");
-        
-        })
   }
  
-//the nextProps will be the fileName of the new file uploaded sent via props from MyMenu.
 componentWillReceiveProps(nextProps)
 {
-  //alert("nextProps: "+nextProps.props);
-  //alert("props now: "+this.props.props);
  if(nextProps.props!==this.props.props)
  {
-   //alert("Not equal");
-  var data = {
-    hvfldrid: getLoggedInUser().rtpthid
-    } 
-    var arrayFiles =  [{}];
-    var arrayMix = [{}];
-     getDetailsofFolders(data).then( (tableData) => {arrayFiles = tableData[0];} )
-     getDetailsofFiles(data).then( (fileData) => {
-      var file=fileData[0];
-      arrayMix = arrayFiles.concat(file);
-     // alert("before the set state in willRecieve");
-      //this setState triggers Render()
-      this.setState({ TData: arrayMix} ); 
-     })
+  this.loadTable(getLoggedInUser().rtpthid);
 }
 }
 
@@ -123,7 +102,6 @@ componentWillReceiveProps(nextProps)
             <TableRow selectable={true} onCellClick={this.thefn}>
             <TableHeaderColumn > </TableHeaderColumn>
                 <TableHeaderColumn >Name</TableHeaderColumn>
-                
                 <TableHeaderColumn>Owner</TableHeaderColumn>
                 <TableHeaderColumn>Last modified by me</TableHeaderColumn>
                 <TableHeaderColumn>Size</TableHeaderColumn>
